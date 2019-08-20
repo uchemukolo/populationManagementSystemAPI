a234a093ac58f93044f47c0519785fc440b0d333
uchemukolo <uchek7@gmail.com>
2019-08-20 23:27:14 +0100

    ch-setup babel and server

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const port = parseInt(process.env.PORT, 10) || 9000;
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({
+  credentials: true,
+}));
+
+
+app.get('*', (request, response) => response.status(200).send({
+  message: 'Welcome to Population Management API!!!',
+}));
+
+
+app.listen(port, () => console.log(`server is up and running on localhost: ${port}`));
+
+export default app;
